b8dc6974c368a9feeded30f7010e39aa13a5d21c
Vitor Ramalho <vitorlramalho@gmail.com>
2023-01-31 11:59:49 -0300

    FirstCommi

--- ExNodeExpress/rotas.js
+++ ExNodeExpress/rotas.js
@@ -1,1 +1,25 @@
-var express = require("express") 
+//Rotas!!!
+
+var express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Seje Bem-vindo ao meu app!");
+});
+
+app.get("/Sobre", function(req, res){
+    res.send("Sobre o meu app")
+})
+
+app.get("/blog", function(req, res){
+    res.send("Meu Blog pessoal")
+})
+
+app.get("/Ola/:nome/:cargo/:idade", function(req,res){
+    res.send("<h1> Ola " +req.params.nome+ "</h1>"+"<h2> Seu cargo é: "+req.params.cargo+"</h2>"+"<h3> Sua idade é: "+req.params.idade+"</h3>");
+   
+})
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081")
+});
