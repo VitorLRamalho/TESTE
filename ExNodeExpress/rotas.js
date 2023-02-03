e7341ce8ccfc1df266a888c90188862212704109
Vitor Ramalho <vitorlramalho@gmail.com>
2023-02-03 13:46:37 -0300

    edit

--- ExNodeExpress/rotas.js
+++ ExNodeExpress/rotas.js
@@ -1,13 +1,12 @@
-var express = require("express");
+const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const Sequelize = require('sequelize');
 
 // Config
 // Template Engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 
 //Conexão com "BD" MySQL
 const sequelize = new Sequelize('teste', 'root', 'Templo@2000', {
@@ -15,7 +14,16 @@ const sequelize = new Sequelize('teste', 'root', 'Templo@2000', {
     dialect: 'mysql'
 });
 
+//Rotas
+app.get('/form', function(req, res){
+  res.render('formulario')
+});
 
-app.listen(8081, function () {
-    console.log("Servidor Rodando na url http://localhost:8081")
+app.post('/sendForm', function(req, res){
+  res.send('Formulario Recebido!!!')
+});
+
+
+app.listen(8080, function () {
+    console.log("Servidor Rodando na url http://localhost:8080")
 });
